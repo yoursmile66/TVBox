7eb782b071b866f8765cfb4a23bb757f312dc528
yoursmile66 <Yoursmile777@outlook.com>
2025-01-24 14:15:27 +0800

    Add files via upload

--- js/JRKAN直播.js
+++ js/JRKAN直播.js
@@ -1,23 +1,12 @@
-// 道长 drpy仓库 https://gitcode.net/qq_32394351/dr_py
-// 道长 drpy安卓本地搭建说明 https://gitcode.net/qq_32394351/dr_py/-/blob/master/%E5%AE%89%E5%8D%93%E6%9C%AC%E5%9C%B0%E6%90%AD%E5%BB%BA%E8%AF%B4%E6%98%8E.md
-// 道长 drpy写源 模板规则说明 https://gitcode.net/supertlo/dr_py#%E6%A8%A1%E6%9D%BF%E8%A7%84%E5%88%99%E8%AF%B4%E6%98%8E
-// 道长 drpy写源 套模模版 https://gitcode.net/qq_32394351/dr_py/-/raw/master/js/%E6%A8%A1%E6%9D%BF.js
-// 道长 drpy写源 影片教程 http://101.34.67.237:5244/%E6%95%99%E8%82%B2/drpy
-// 道长 drpy写源 影片教程(m3u8切片) https://freedrpy.run.goorm.io/txt/jc/playlist.m3u8
-// 海阔下载 https://haikuo.lanzoui.com/u/GoldRiver
-// Pluto Player官方TG https://t.me/PlutoPlayer
-// Pluto Player官方TG https://t.me/PlutoPlayerChannel
-
-
 var rule = {
     title:'JRKAN直播',
-    host:'http://m.jrkan2023.com/',
-	// JRKAN备用域名:m.jrskan8.com / m.jrkan666.com / jryyds.com / jrsbxj.com
-	// JRKAN网址发布:qiumi1314.com
+    host:'http://www.jrs80.com/?lan=1',
+    // JRKAN备用域名:www.jrkankan.com / www.jrkan365.com / jrsyyds.com / www.jryyds.com / jrskan.com / jrsbxj.com /Jrkan备用域名1: www.jrkan2022.com 备用域名2: www.jrs23.com 备用域名2: www.jrskk.com 最新网址发布:www.qiumi1314.co
+    
     url:'/fyclass',
-    searchUrl:'',
-    searchable:0,
-    quickSearch:0,
+    searchUrl:'/x/search/?q=**',
+    searchable:1,
+    quickSearch:1,
     class_name:'全部',
     class_url:'/',
     //class_url:'?live',
@@ -26,14 +15,19 @@ var rule = {
     },
     timeout:5000,
     play_parse:true,
-    lazy:'',
+    lazy:"",
     limit:6,
     double:false,
     推荐:'*',
-    // 一级播放线路x3 可自行切换
-    //一级:'.loc_match .d-touch;li&&Text;img&&src;.lab_time&&Text;a:eq(0)&&href',//play.sportsteam365.com
-    一级:'.loc_match:eq(2) ul;li:gt(1):lt(4)&&Text;img&&src;li:lt(2)&&Text;a:eq(1)&&href',//play.sportsteam333.com
-    //一级:'.loc_match .d-touch;li&&Text;img&&src;.lab_time&&Text;a:eq(2)&&href',//play.sportsteam666.com
-    二级:{title:'.sub_list li:lt(2)&&Text;.sub_list li:eq(0)&&Text',img:'img&&src',desc:';;;.lab_team_home&&Text;.lab_team_away&&Text',content:'.sub_list ul&&Text',tabs:'',tab_text:'',lists:'.sub_channel a',list_text:'a&&Text',list_url:'a&&data-play'},
-    搜索:'',
+    // 一级:'.loc_match:eq(2) ul;li:gt(1):lt(4)&&Text;img&&src;li:lt(2)&&Text;a:eq(1)&&href',//play.sportsteam333.com
+    一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.d-touch');tabs.forEach(function(it){var pz=pdfh(it,'.name:eq(1)&&Text');var ps=pdfh(it,'.name:eq(0)&&Text');var pk=pdfh(it,'.name:eq(2)&&Text');var img=pd(it,'img&&src');var url=pd(it,'a.me&&href');var timer=pdfh(it,'.lab_time&&Text');var parts = timer.split(' ');var dateParts = parts[0].split('-');var timeParts = parts[1].split(':');var year = new Date().getFullYear();var date = new Date(year, dateParts[0] - 1, dateParts[1], timeParts[0], timeParts[1]);var now = new Date();var hundredMinutesAgo = new Date(now.getTime() - 100 * 60 * 1000);if (date > hundredMinutesAgo) {timer = timer.split(' ')[1];items.push({desc:timer+ '  '+'🏆'+ps,title:pz+'  🆚  '+pk,pic_url:img,url:url})}});setResult(items);",
+    二级:{
+        "title":".sub_list li:lt(2)&&Text;.sub_list li:eq(0)&&Text",
+        "img":"img&&src",
+        "desc":";;;.lab_team_home&&Text;.lab_team_away&&Text",
+        "content":".sub_list ul&&Text",
+        "tabs":"js:TABS=['实时直播']",
+        "lists":"js:LISTS=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;let html=request(input);let data=pdfa(html,'.sub_playlist&&a');TABS.forEach(function(tab){let d=data.map(function(it){let name=pdfh(it,'strong&&Text');let url=pd(it,'a&&data-play');return name+'$'+url});LISTS.push(d)});",
+    },
+    搜索:'js:let d=[];setResult(d);',
 }
